aee4b3b6e0be26d0cad55e1eb0fde896525c435b
Jon <jonathan.martinez@maxikash.com>
2025-10-30 09:11:49 -0600

    Cambios

--- app.py
+++ app.py
@@ -427,4 +427,8 @@ def nivel_jerarquico():
 # Run App
 # -----------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
